6eecab534ad796ce1064c45aef4f5a2a2111b710
Denis-Omingo <omingodenis7@gmail.com>
2024-05-19 11:19:58 +0300

    creating a route to create a restaurant.Including file upload using cloudinary and validation.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute';
+import {v2 as clodinary} from 'cloudinary';
+import myRestaurantRoute from './routes/MyRestaurantRoute';
 
 
     mongoose.connect(process.env.MONGODB as string)
@@ -10,6 +12,11 @@ import myUserRoute from './routes/MyUserRoute';
         console.log('Connected to the database')
     })
     .catch((error)=>console.log(error))
+clodinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLODINARY_API_SECRET,
+});
 
 
 const app=express();
@@ -21,6 +28,7 @@ app.get("/health",async(req:Request,res:Response)=>{
 })
 
 app.use("/api/my/user",myUserRoute)
+app.use("/api/my/restaurant",myRestaurantRoute)
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
