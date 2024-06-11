e887b3c07989d8665b8f6519fffded1152839332
Denis-Omingo <omingodenis7@gmail.com>
2024-06-11 13:12:40 +0300

    adding manage restaurant, order status, and checkout functionalities

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
-import express, {Request,Response} from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import myUserRoute from './routes/MyUserRoute';
-import {v2 as clodinary} from 'cloudinary';
-import myRestaurantRoute from './routes/MyRestaurantRoute';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
+mongoose
+  .connect(process.env.MONGODB as string)
+  .then(() => console.log("Connected to database!"));
 
-    mongoose.connect(process.env.MONGODB as string)
-    .then(()=>{
-        console.log('Connected to the database')
-    })
-    .catch((error)=>console.log(error))
-clodinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLODINARY_API_SECRET,
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLODINARY_API_SECRET,
 });
 
+const app = express();
 
-const app=express();
-app.use(express.json());
 app.use(cors());
 
-app.get("/health",async(req:Request,res:Response)=>{
-    res.send({message:"Health ok!"});
-})
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
-app.use("/api/my/user",myUserRoute)
-app.use("/api/my/restaurant",myRestaurantRoute)
+app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
+
+app.listen(3000, () => {
+  console.log("server started on localhost:3000");
+});
